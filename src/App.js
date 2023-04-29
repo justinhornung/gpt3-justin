b3869cad356ebfd343cd917c2caa3abb42ed9962
justinhornung <jhornung328i@gmail.com>
2023-04-29 11:19:58 -0400

    Add and style Navbar links

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./contain
 import { CTA, Brand, Navbar } from "./components";
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
