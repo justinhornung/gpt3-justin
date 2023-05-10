395333f6774de231c840d2a0506286cd60ee003e
justinhornung <jhornung328i@gmail.com>
2023-05-09 21:44:15 -0400

    Add Possibility JSX and CSS

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import possibilityImage from "../../assets/possibility.png";
 import "./possibility.css";
 
-const Possibility = () => {
-  return <div>Possibility</div>;
-};
+const Possibility = () => (
+  <div className="gpt3__possibility section__padding" id="possibility">
+    <div className="gpt3__possibility-image">
+      <img src={possibilityImage} alt="possibility" />
+    </div>
+    <div className="gpt3__possibility-content">
+      <h4>Request Early Access To Get Started</h4>
+      <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+      <p>
+        Proident eu labore est do dolor excepteur esse mollit minim. Velit Lorem voluptate dolore commodo duis tempor
+        anim culpa mollit magna ad. Adipisicing ipsum ut exercitation velit consequat. Enim magna sit dolore enim est
+        culpa Lorem. Deserunt dolor id non duis cupidatat adipisicing.
+      </p>
+      <h4>Request Early Access To Get Started</h4>
+    </div>
+  </div>
+);
 
 export default Possibility;
